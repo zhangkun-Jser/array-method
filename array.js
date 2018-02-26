7874dda9208c2e8e7af77602dc0e70aabd2f17d2
zk <978895036@qq.com>
2018-02-26 23:52:58 +0800

    radom

--- array.js
+++ array.js
@@ -1,10 +1,24 @@
 //数组去重
-function deleteRepeat(arr){
-  let a=[]
-  for(let i of arr){
-    if(a.indexOf(i)==-1){
+function deleteRepeat(arr) {
+  let a = []
+  for (let i of arr) {
+    if (a.indexOf(i) == -1) {
       a.push(i)
     }
   }
   return a
+}
+//生成指定区间随机4位数
+function random(min, max) {
+  return [0, 0, 0, 0].map(() => Math.floor(Math.random() * (max - min + 1) + min))
+}
+//
+function a(a, b) {
+  let c = []
+  for (let i of a) {
+    if (b.indexOf(i) != -1) {
+      c.push(i)
+    }
+  }
+  return c
 }
